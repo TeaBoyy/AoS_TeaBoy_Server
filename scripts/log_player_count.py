58c3a5de97c91fd05d57bc30bfae19bd0dbce2ec
TeaBoy <38662941+TeaBoyy@users.noreply.github.com>
2023-07-01 16:07:52 +0300

    Implement log_player_count.py script to dump player count in file on each connect/disconnect

---
+++ scripts/log_player_count.py
@@ -0,0 +1,34 @@
+# Logs player count whenever player joins/disconnects
+
+from datetime import datetime
+
+LOG_FILE_PATH="player_count_log.txt"
+
+def apply_script(protocol, connection, config):
+    class LogPlayerCountConnection(connection):
+        logfile = None
+        logfile_opened = False
+
+        def on_connect(self):
+            self.log_player_count()
+            return connection.on_connect(self)
+        
+        def on_disconnect(self):
+            self.log_player_count()
+            return connection.on_disconnect(self)
+        
+        def log_player_count(self):
+            player_count = len(self.protocol.connections)
+            current_date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            log_entry = "[" + current_date + "] " + "Current player count: " + str(player_count) + ".\n"
+
+            if not self.logfile_opened:
+                self.logfile = open(LOG_FILE_PATH, "a")
+
+            if self.logfile is not None:
+                self.logfile.write(log_entry)
+                self.logfile.flush()
+            else:
+                print(log_entry)
+
+    return protocol, LogPlayerCountConnection
