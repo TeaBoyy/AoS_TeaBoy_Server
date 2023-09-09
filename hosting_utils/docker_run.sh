e088df2352a7539365043199aab6696ad5945f85
TeaBoy <38662941+TeaBoyy@users.noreply.github.com>
2023-09-09 15:40:09 +0300

    Remove hosting_utils, no use in wine when can build natively

--- hosting_utils/docker_run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo docker run -d -u root -v $(dirname $PWD):/home/container -p 32887:32887/tcp -p 32887:32887/udp -i -t hcgcloud/pterodactyl-images:ubuntu-wine ./hosting_utils/run.sh
--- hosting_utils/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Starting server in endless loop... (Press Ctrl+C or kill process with <PID: $$> to stop)"
-
-while [[ true ]]; do
-    wine64 ./run.exe
-done
