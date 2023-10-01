3003a76ad37f52aab2a442f9678040f8b828a643
TeaBoy <38662941+TeaBoyy@users.noreply.github.com>
2023-10-01 19:30:38 +0300

    Report tent coords when team begins capturing it
    Every time opposite team tries to capture tent/objective, coords (on the grid) are displayed in chat. Leaving or dying also logs similar message. No stalemate detection, so it's just considered as leaving/dying

--- scripts/tc_mode_extension.py
+++ scripts/tc_mode_extension.py
@@ -2,6 +2,56 @@ from pyspades.constants import MAX_TERRITORY_COUNT, MIN_TERRITORY_COUNT, TC_MODE
 from pyspades.server import Territory
 
 def apply_script(protocol, connection, config):
+    def convert_entity_pos_to_map_label(pos = (0.0, 0.0)):
+        x, y = pos
+
+        map_chunks_count = 8
+        map_chunk_size = 512 / map_chunks_count
+
+        x_index = int(x / map_chunk_size)
+        y_index = int(y / map_chunk_size)
+
+        if x_index >= map_chunks_count:
+            x_index = map_chunks_count - 1
+        elif x_index < 0:
+            x_index = 0
+
+        if y_index >= map_chunks_count:
+            y_index = map_chunks_count - 1
+        elif y_index < 0:
+            y_index = 0
+
+        return "" + chr(ord('A') + x_index) + "" + chr(ord('1') + y_index)
+    
+    class TCExtensionTerritory(Territory):
+        def send_tent_map_label_to_chat(self, team, isTaking = True, pos = (0.0, 0.0)):
+            if team is None:
+                team_str = "Unknown"
+            elif team.id == 0:
+                team_str = "Blue"
+            elif team.id == 1:
+                team_str = "Green"
+            else:
+                team_str = "Unknown"
+
+            chat_message = team_str + " team is "
+
+            if not isTaking:
+                chat_message += "no longer "
+
+            chat_message += "taking objective " + convert_entity_pos_to_map_label(pos) + "!"
+            self.protocol.send_chat(chat_message)
+
+        def update_rate(self):
+            had_finish_call = self.finish_call != None
+            Territory.update_rate(self)
+            has_finish_call = self.finish_call != None
+
+            if not had_finish_call and has_finish_call:
+                self.send_tent_map_label_to_chat(self.capturing_team, True,  (self.x, self.y))
+            elif had_finish_call and not has_finish_call:
+                self.send_tent_map_label_to_chat(self.capturing_team, False,  (self.x, self.y))
+
     class TCExtensionProtocol(protocol):
         game_mode = TC_MODE
         
@@ -45,7 +95,7 @@ def apply_script(protocol, connection, config):
                 y1 += offset_y
                 y2 -= offset_y
 
-                flag = Territory(i, self, *self.get_random_location(
+                flag = TCExtensionTerritory(i, self, *self.get_random_location(
                     zone = (x1, y1, x2, y2)))
                 if i < territory_count / 2:
                     team = self.blue_team
