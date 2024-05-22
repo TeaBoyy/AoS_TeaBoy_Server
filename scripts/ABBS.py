d725970ee8ec5311dbfcc1f0279ea86eb4f1c940
TeaBoy <38662941+TeaBoyy@users.noreply.github.com>
2024-05-22 12:00:06 +0300

    Disabled LITE_MODE

--- scripts/ABBS.py
+++ scripts/ABBS.py
@@ -58,7 +58,7 @@ try:
     # TODO: but stil can't join team with bots and 1 human, need to join team with 0 humans first
     BOT_ADD_PATTERN = 2
 
-    LITE_MODE       = True    
+    LITE_MODE       = False    
     LV_AUTO_ADJUST  = 0       # 0:off 1:human vs bot   2:blue vs green 3:both human/bot and blue/green
     BOT_NUM_NAME    = True    
     CPU_LV          = [16,12]  
