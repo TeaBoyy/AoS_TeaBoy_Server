c64e96607d2aee7b2341df2c77aae8f4ae5a0838
TeaBoy <38662941+TeaBoyy@users.noreply.github.com>
2024-06-20 12:08:45 +0300

    WIP - spawning behind frontline

--- scripts/frontline.py
+++ scripts/frontline.py
@@ -12,6 +12,8 @@ def apply_script(protocol, connection, config):
         game_mode = TC_MODE
         
         tents_grid = []
+        grid_width = 4
+        grid_height = 3
 
         blue_team_home_bases = []
         green_team_home_bases = []
@@ -27,20 +29,17 @@ def apply_script(protocol, connection, config):
                 print("Warning: missing tents grid or home bases list")
             
         def get_cp_entities(self): 
-            grid_width = 4
-            grid_height = 3
-
-            if (grid_width * grid_height > MAX_ENTITIES_COUNT):
+            if (self.grid_width * self.grid_height > MAX_ENTITIES_COUNT):
                 print("Error: exceeding {} tents limit".format(MAX_ENTITIES_COUNT))
                 return []
 
             extra_fallback_tent_count = 1
 
-            offset_x = 512 / (grid_width + extra_fallback_tent_count)
-            offset_y = 512 / (grid_height + extra_fallback_tent_count)
+            offset_x = 512 / (self.grid_width + extra_fallback_tent_count)
+            offset_y = 512 / (self.grid_height + extra_fallback_tent_count)
 
-            for i in range(grid_height):
-                self.tents_grid.append([None for i in range(grid_width)])
+            for i in range(self.grid_height):
+                self.tents_grid.append([None for i in range(self.grid_width)])
 
             # Skip one tent distance
             start_x = offset_x
@@ -48,18 +47,16 @@ def apply_script(protocol, connection, config):
 
             entities = []
             counter = 0 
-            for x in range(0, grid_width):
-                for y in range(0, grid_height):
-
+            for x in range(0, self.grid_width):
+                for y in range(0, self.grid_height):
                     point_x = start_x + offset_x * x
                     point_y = start_y + offset_y * y
 
-
                     tent = Territory(counter, self, *(point_x, point_y, self.map.get_z(point_x, point_y)))
-                    if x < grid_width / 2:
+                    if x < self.grid_width / 2:
                         tent.team = self.blue_team
                         self.blue_team_home_bases.append(tent)
-                    elif x > (grid_width - 1) / 2:
+                    elif x > (self.grid_width - 1) / 2:
                         tent.team = self.green_team
                         self.green_team_home_bases.append(tent)
                     else:
@@ -71,5 +68,39 @@ def apply_script(protocol, connection, config):
                     counter += 1
 
             return entities
+
+        # TODO:
+        def get_valid_spawn_points(self, team):
+            valid_spawn_points = []
+            if team == None:
+                return valid_spawn_points
+
+            for x in range(0, self.grid_width):
+                for y in range(0, self.grid_height):
+                    tent = self.tents_grid[x][y]
+
+                    if tent.team != team:
+                        continue
+
+                    # TODO: if own tent, check if got any enemy tents neighbors
+                    print("test")
+
+        # TODO:
+        def is_tent_a_frontline(self, x, y):
+            left, right, down, up = None
+            
+            if x > 0:
+                left = self.tents_grid[x - 1][y]
+
+            if x < self.grid_width - 1:
+                right = self.tents_grid[x - 1][y]
+
+            if y > 0:
+                down = self.tents_grid[x][y + 1]
+
+             if y < self.grid_height - 1:
+                up = self.tents_grid[x][y - 1]
+
+            
 
     return FrontlineProtocol, FrontlineConnection
