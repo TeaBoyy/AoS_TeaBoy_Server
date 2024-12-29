f0f9fac7fa29977310ad54cb6faf991c96efaeef
TeaBoy <38662941+TeaBoyy@users.noreply.github.com>
2024-12-29 13:36:52 +0200

    [WIP] Force all bots to push when fighting
    With rifle only ofc

--- scripts/ABBS.py
+++ scripts/ABBS.py
@@ -1011,7 +1011,7 @@ try:
                 self.battlecrouching=False
                 if weapon_rdm>0.4:
                     self.set_weapon(RIFLE_WEAPON, True)
-                    self.battle_distance = uniform(30,110)
+                    self.battle_distance = 20
                     if random.random()<0.2:
                         self.battlecrouching=True
                     if weapon_rdm>0.7 or random.random()<self.cpulevel:
